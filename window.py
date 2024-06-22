792626e367da8ea321ad34cb7e0c7f990981a101
DavidIRL <david.white0911@gmail.com>
2024-06-22 01:13:48 -0400

    create Point & Line classes

--- window.py
+++ window.py
@@ -29,3 +29,22 @@ class Window:
 
     def close(self):
         self.running = False
+
+
+class Point:
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+
+class Line:
+    def __init__(self, point1, point2):
+        self.p1 = point1
+        self.p2 = point2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
+
+
